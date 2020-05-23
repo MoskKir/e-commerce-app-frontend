bccc2c2b631312ffba2aabf8474c6cb4be650013
Kirill Moskalevich <mosk9364@gmail.com>
2020-05-23 04:10:27 +0300

    refactor: changed app arhitecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,16 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { ProductPageComponent } from './product-page/product-page.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { QuillModule } from 'ngx-quill';
-import { ProductCardComponent } from './product-card/product-card.component';
-import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UserLoginPageComponent } from './user-login-page/user-login-page.component';
 import { StoreModule, ActionReducer, MetaReducer } from '@ngrx/store';
 import { environment } from 'src/environments/environment';
 import { EffectsModule } from '@ngrx/effects';
-import { AuthCustomersService } from './shared/auth-customers.service';
-import { AuthEffects } from './shared/store/user/auth.effects';
-import { reducers } from './shared/store/app.states';
-import { AuthModule } from './shared/store/auth.module';
-import { CustomerMainMenuComponent } from './customer-main-menu/customer-main-menu.component';
-import { TokenInterceptor } from './shared/auth-customers-token.interceptor';
+import { AuthCustomersService } from './shop/services/auth-customers.service';
+import { AuthEffects } from './shop/shared/store/user/auth.effects';
+import { reducers } from './shop/shared/store/app.states';
+import { TokenInterceptor } from './shop/services/auth-customers-token.interceptor';
 
 export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
   return localStorageSync({keys: ['auth'], rehydrate: true})(reducer);
@@ -35,14 +26,6 @@ const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
 @NgModule({
   declarations: [
     AppComponent,
-    MainLayoutComponent,
-    MainPageComponent,
-    ProductPageComponent,
-    CartPageComponent,
-    ProductCardComponent,
-    SignUpPageComponent,
-    UserLoginPageComponent,
-    CustomerMainMenuComponent
   ],
   imports: [
     BrowserModule,
@@ -54,14 +37,12 @@ const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
     StoreModule.forRoot(
       reducers,
       {metaReducers}
-      ),
+    ),
     EffectsModule.forRoot([AuthEffects]),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
       logOnly: environment.production,
     }),
-    AuthModule,
-    ClickOutsideModule,
   ],
   providers: [
     AuthCustomersService,
