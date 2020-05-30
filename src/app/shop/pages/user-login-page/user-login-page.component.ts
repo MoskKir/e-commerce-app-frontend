797a206c47223716fdf2463e3e80633fa1b8a7c8
Kirill Moskalevich <mosk9364@gmail.com>
2020-05-30 21:35:31 +0300

    feat: error heandler

--- src/app/shop/pages/user-login-page/user-login-page.component.ts
+++ src/app/shop/pages/user-login-page/user-login-page.component.ts
@@ -4,8 +4,9 @@ import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 
 import { AuthCustomersService } from '../../services/auth-customers.service';
-import { AppState } from '../../shared/store/app.states';
+import { AppState, selectAuthState } from '../../shared/store/app.states';
 import { LogIn } from '../../shared/store/user/auth.actions';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user-login-page',
@@ -14,27 +15,29 @@ import { LogIn } from '../../shared/store/user/auth.actions';
 })
 export class UserLoginPageComponent implements OnInit {
   form: FormGroup;
-  submitted = false;
+  getState: Observable<any>;
+  errorMessage: string | null;
 
   constructor(
     public auth: AuthCustomersService,
     private router: Router,
     private store: Store<AppState>
-  ) { }
+  ) {
+    this.getState = this.store.select(selectAuthState);
+  }
 
   ngOnInit() {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
-    })
+    });
+
+    this.getState.subscribe((state) => {
+      this.errorMessage = state.errorMessage;
+    });
   }
 
   submit(): void {
-    if (this.form.invalid) {
-      return;
-    }
-
-    this.submitted = true;
 
     const user = {
       email: this.form.value.email,
--- src/app/shop/services/auth-customers-token.interceptor.ts
+++ src/app/shop/services/auth-customers-token.interceptor.ts
@@ -1,10 +1,12 @@
 import { Injectable, Injector } from '@angular/core';
 import {
-  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse
 } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 import { AuthCustomersService } from './auth-customers.service';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs/operators';
 
 
 @Injectable({
@@ -19,10 +21,26 @@ export class TokenInterceptor implements HttpInterceptor {
     request = request.clone({
       setHeaders: {
         // 'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/json'
       }
     });
     return next.handle(request);
+  }
+}
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    return next.handle(request).pipe(
+      catchError((response: any) => {
+        if (response instanceof HttpErrorResponse && response.status === 400) {
+          console.log(response);
+        }
+        return Observable.throw(response);
+      })
+    )
   }
 }
 
